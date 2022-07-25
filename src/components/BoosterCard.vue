06c260fae62800eaf5d7acb6273349fbb1875ec5
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-07-25 13:57:51 -0700

    build initial RandomView component

--- src/components/BoosterCard.vue
+++ src/components/BoosterCard.vue
@@ -1,4 +1,15 @@
+<script setup>
+    import { defineProps } from "vue"
+    const props = defineProps({
+        text: String,
+        type: String
+        })
+</script>
+
 <template>
+    <div class="booster-card">
+        <h3>{{text}}</h3>
+    </div>
 </template>
 
 <style scoped>
