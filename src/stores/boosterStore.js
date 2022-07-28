190562ac9aa961badbc6873f66eea0caf7013cbe
Cyanne Jones <jonescyanne@yahoo.com>
2022-07-28 16:12:41 -0600

    Refactor for syntax and console logs

--- src/stores/boosterStore.js
+++ src/stores/boosterStore.js
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export const useBoosterStore = defineStore({
   id: 'boosters',
@@ -7,31 +7,30 @@ export const useBoosterStore = defineStore({
   }),
   getters: {
     favoritedFacts: (state) => {
-      return state.boosters.filter(booster => booster.type === "fact")
+      return state.boosters.filter(booster => booster.type === "fact");
     },
     favoritedDrinks: (state) => {
-      return state.boosters.filter(booster => booster.type === "drink")
+      return state.boosters.filter(booster => booster.type === "drink");
     },
     favoritedJokes: (state) => {
-      return state.boosters.filter(booster => booster.type === "joke")
+      return state.boosters.filter(booster => booster.type === "joke");
     }
   }, 
   actions: {
     favoriteBooster(booster){
-      console.log(booster)
-      booster.isFavorited = true
-      this.boosters.push(booster)
-      const boosterAsString = JSON.stringify(booster)
-      localStorage.setItem(booster.id, boosterAsString)
+      booster.isFavorited = true;
+      this.boosters.push(booster);
+      const boosterAsString = JSON.stringify(booster);
+      localStorage.setItem(booster.id, boosterAsString);
     },
     deleteBooster(deletedBooster){
-      deletedBooster.isFavorited = false
-      localStorage.removeItem(deletedBooster.id)
-      this.boosters = this.boosters.filter(booster => booster.id !== deletedBooster.id)  
+      deletedBooster.isFavorited = false;
+      localStorage.removeItem(deletedBooster.id);
+      this.boosters = this.boosters.filter(booster => booster.id !== deletedBooster.id);
     },
     loadFromStorage(){
-      this.boosters = Object.values(localStorage).map(object => JSON.parse(object))
-      this.boosters.sort((a,b) => a.id - b.id)
+      this.boosters = Object.values(localStorage).map(object => JSON.parse(object));
+      this.boosters.sort((a,b) => a.id - b.id);
     }
   }
-})
+});
