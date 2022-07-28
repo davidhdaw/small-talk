bcc7b3807be56586b726d5c5e8f18230dd817ae2
Cyanne Jones <jonescyanne@yahoo.com>
2022-07-28 16:13:56 -0600

    Refactor for syntax and indentation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import { useBoosterStore } from './stores/boosterStore';
-const { loadFromStorage } = useBoosterStore();
-loadFromStorage()
+  import { RouterLink, RouterView } from "vue-router";
+  import { useBoosterStore } from './stores/boosterStore';
+  const { loadFromStorage } = useBoosterStore();
+  loadFromStorage();
 </script>
 
 <template>
@@ -27,60 +27,60 @@ loadFromStorage()
 
 <style scoped>
 
-h1 {
-    -webkit-text-stroke: 1px black;
-    font-size: 40px;
+  h1 {
+      -webkit-text-stroke: 1px black;
+      font-size: 40px;
+      margin: 0px;
+      text-shadow: 3px 3px #101010;
+  }
+  .small {
+    font-size: 30px;
+  }
+
+  header {
+    line-height: 1.5;
+  }
+
+  p {
     margin: 0px;
-    text-shadow: 3px 3px #101010;
-}
-.small {
-  font-size: 30px;
-}
+    -webkit-text-stroke: 0.05rem black;
+    font-size: 1.2rem;
+  }
 
-header {
-  line-height: 1.5;
-}
-
-p {
-  margin: 0px;
-  -webkit-text-stroke: 0.05rem black;
-  font-size: 1.2rem;
-}
-
-.router-link {
-  margin: 1rem;
-  font-size: 2rem;
-  background-color: rgba(131, 145, 163, 0.7);
-  box-shadow: 0.5rem 0.5rem #000000;
-  border: 0.2rem solid black;
-  padding: 0.5rem;
-  border-radius: 1rem;
-}
-nav {
-  width: 100%;
-  text-align: center;
-  display: flex;
-  font-size: 1rem;
-}
-
-nav a.router-link-exact-active {
-  background-color: rgb(15, 142, 196);
-  text-shadow: 2px 2px #ff0000;
-}
-
-nav a:hover {
-  background-color: #E0676D;
-}
-
-nav a {
-  display: inline-block;
-  -webkit-text-stroke: 0.05rem black;
-  font-size: 20px;
-}
-
-@media screen and (max-width: 500px) {
   .router-link {
+    margin: 1rem;
+    font-size: 2rem;
+    background-color: rgba(131, 145, 163, 0.7);
+    box-shadow: 0.5rem 0.5rem #000000;
+    border: 0.2rem solid black;
+    padding: 0.5rem;
+    border-radius: 1rem;
+  }
+  nav {
+    width: 100%;
+    text-align: center;
+    display: flex;
     font-size: 1rem;
   }
-}
+
+  nav a.router-link-exact-active {
+    background-color: rgb(15, 142, 196);
+    text-shadow: 2px 2px #ff0000;
+  }
+
+  nav a:hover {
+    background-color: #E0676D;
+  }
+
+  nav a {
+    display: inline-block;
+    -webkit-text-stroke: 0.05rem black;
+    font-size: 20px;
+  }
+
+  @media screen and (max-width: 500px) {
+    .router-link {
+      font-size: 1rem;
+    }
+  }
 </style>
